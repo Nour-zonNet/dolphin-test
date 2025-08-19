5fafaf7bdb966dc2fa652bea0ce81b83e693cdf4
Nour-zonNet <nanter@z0n.net>
2025-08-19 11:31:22 +0300

    global colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { CustomArrowIcon } from "./assets/icons";
 import { useTranslation } from "react-i18next";
 import "./i18n"; 
+import Packages from "./pages/Packages";
 
 function App() {
   const { t, i18n } = useTranslation();
   return (
-    <div className="flex gap-4">
-      <h1>{ t("welcome") }</h1>
-      <CustomArrowIcon />
-      <CustomArrowIcon size={40} color="blue" />
-      <CustomArrowIcon size={40} color="#00C853" />
+    <div>
+      <Packages />
     </div>
   );
 }
---
+++ src/components/cards/PackageCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PackageCard = () => {
+  return (
+    <div class="bg-[var(--color-health)] text-[var(--color-quran)] p-4">
+      Hello 
+    </div>
+  )
+}
+
+export default PackageCard
