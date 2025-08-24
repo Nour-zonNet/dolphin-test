469e3c1be6e33c1876dcabf0a9fdc9916a5ec149
Nour-zonNet <nanter@z0n.net>
2025-08-24 10:29:18 +0300

    refactoring

--- src/features/home/components/Navbar.jsx
+++ src/features/home/components/Navbar.jsx
@@ -5,15 +5,15 @@ const Navbar = () => {
     <header className="flex items-center justify-between px-6 py-4">
       {/* Logo */}
       <div className="flex items-center gap-2">
-        <img src={dolphinLogo} alt="logo" className="h-10" />
+        <img src={dolphinLogo} alt="logo" className="h-20" />
       </div>
 
       {/* Nav buttons */}
       <div className="flex gap-3">
-        <button className="flex items-center gap-1 border border-orangedeep rounded-full px-4 py-2 text-sm font-medium text-[#0C2D40] hover:bg-gray-50">
+        <button className="flex items-center gap-1 border border-orangedeep hover:bg-orangedeep focus:bg-orangedeep transition rounded-full px-4 py-2 text-sm font-medium text-[#0C2D40] cursor-pointer">
           سياسة الخصوصية
         </button>
-        <button className="flex items-center gap-1 border border-orangedeep rounded-full px-4 py-2 text-sm font-medium text-[#0C2D40] hover:bg-gray-50">
+        <button className="flex items-center gap-1 border border-orangedeep hover:bg-orangedeep focus:bg-orangedeep transition rounded-full px-4 py-2 text-sm font-medium text-[#0C2D40] cursor-pointer">
           عرض الباقات
         </button>
       </div>
