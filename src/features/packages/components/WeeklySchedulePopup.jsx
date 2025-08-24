86557ea3e476caa88676511eefacc8d66db26cde
Nour-zonNet <nanter@z0n.net>
2025-08-24 14:54:40 +0300

    rounded border

--- src/features/packages/components/WeeklySchedulePopup.jsx
+++ src/features/packages/components/WeeklySchedulePopup.jsx
@@ -40,10 +40,10 @@ const WeeklySchedulePopup = ({ open, setOpen }) => {
           {/* Schedule Table */}
           <div className="overflow-x-auto">
             <table className="w-full text-center border-collapse min-w-[500px]">
-              <thead className="rounded">
-                <tr className="bg-softblue text-navyteal border border-black rounded">
+              <thead className="">
+                <tr className="bg-softblue text-navyteal">
                   {days.map((day) => (
-                    <th key={day} className="py-2 sm:py-3 font-medium text-sm sm:text-base rounded">
+                    <th key={day} className="py-2 sm:py-3 font-medium text-sm sm:text-base">
                       {day}
                     </th>
                   ))}
