7c3253ba573e10885053a77c481a57db6df46b7a
Mahmoud Gamal <mgamal@z0n.net>
2025-08-25 16:02:13 +0300

    move constants Arrays  to constants folder

---
+++ src/constants/API_ENDPOINTS.js
@@ -0,0 +1,8 @@
+export const ENDPOINTS = {
+  CHECK_PHONE: "/student/check-phone",
+  LOGIN: "/student/login",
+  REGISTER: "/student/register",
+  VERIFY_OTP: "/student/verify",
+  GET_PROFILE: "/auth/me",
+  LOGOUT: "/auth/logout",
+};
