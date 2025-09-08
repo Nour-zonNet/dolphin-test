e549c1c8e3d27da23ae79b8edbf6e2bf7aefaf02
Mahmoud Gamal <mgamal@z0n.net>
2025-09-08 10:37:34 +0300

    handle navigation in mobile bar

--- src/components/layout/MobileNav/MobileNav.jsx
+++ src/components/layout/MobileNav/MobileNav.jsx
@@ -3,19 +3,22 @@ import { ProfileInfo } from "./components";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { ContentIcon } from "../../../utils/icons";
+import { useNavigate } from "react-router-dom";
 
 const MobileNav = () => {
   const { t } = useTranslation();
   const [activeTab, setActiveTab] = useState("home");
   const activeColor = "#1B648E";
   const inactiveColor = "#7A8085";
+  const navigate = useNavigate()
 
   return (
     <nav className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full bg-white shadow-[0_-1px_1px_0_rgba(0,0,0,0.15)] z-50">
       <div className="flex justify-between px-4 sm:px-6 items-center py-2">
         {/* Home */}
         <button
-          onClick={() => setActiveTab("home")}
+          onClick={() =>{ setActiveTab("home"), navigate("/schedule")}}
+
           className="flex flex-col items-center transition cursor-pointer hover:scale-105"
         >
           <Home
