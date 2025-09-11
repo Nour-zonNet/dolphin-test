df045f166b285bd41e4945569b9371c2a9db7c4c
Hassan Ali Tamam <119606428+hassanalitamam@users.noreply.github.com>
2025-09-11 19:04:38 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -36,23 +36,11 @@ export default defineConfig({
           // Component chunks
           if (id.includes("/src/components/")) return "components";
         },
-        inlineDynamicImports: false,
-        preserveModules: false,
-        format: "es",
       },
     },
     chunkSizeWarningLimit: 1000,
-    target: "es2015",
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-      mangle: {
-        safari10: true,
-      },
-    },
+    target: "esnext",
+    minify: "esbuild",
   },
   resolve: {
     alias: {
