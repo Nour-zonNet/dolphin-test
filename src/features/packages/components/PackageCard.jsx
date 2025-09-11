02bc837c1f4ace442900e2f9cf870182fff73380
Mahmoud Gamal <mgamal@z0n.net>
2025-09-11 17:17:55 +0300

    handle errors

--- src/features/packages/components/PackageCard.jsx
+++ src/features/packages/components/PackageCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useCallback } from "react";
 import { useTranslation } from "react-i18next";
-import { STATUS_CONFIG } from "@/constants/STATUS_CONFIG";
+// import { STATUS_CONFIG } from "@/constants/STATUS_CONFIG";
 import {
   Calender,
   TelegramCircle,
