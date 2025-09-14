a23b2483ec081f61a36b2cd4b2e29ea03643c30f
Mahmoud Gamal <mgamal@z0n.net>
2025-09-14 15:09:43 +0300

    handle new tap open when enter session

--- src/components/feedback/modal/modals/StatusModal.jsx
+++ src/components/feedback/modal/modals/StatusModal.jsx
@@ -2,10 +2,10 @@ import { MODAL_TYPES } from "../../../../constants/MODAL_TYPES";
 import { CorrectCircle, Cross } from "../../../../utils/icons";
 import { useTranslation } from "react-i18next";
 import Button from "../../../ui/Button";
-import successImg from "../../../../assets/modal/successModal.svg"
-import failImg from "../../../../assets/modal/warningModal.svg"
+import successImg from "../../../../assets/modal/successModal.svg";
+import failImg from "../../../../assets/modal/warningModal.svg";
 
-const   StatusModal = ({ type, onConfirm, onClose, title, message }) => {
+const StatusModal = ({ type, onConfirm, onClose, title, message }) => {
   const { t } = useTranslation();
 
   return (
@@ -20,14 +20,21 @@ const   StatusModal = ({ type, onConfirm, onClose, title, message }) => {
 
       {/* Image + Head */}
       <div className="flex flex-col items-center mt-6">
-        <img className="w-40 h-auto"  loading="lazy" alt={type} src={type == MODAL_TYPES.SUCCESS  ? successImg : failImg } />
+        <img
+          className="w-40 h-auto"
+          loading="lazy"
+          alt={type}
+          src={type == MODAL_TYPES.SUCCESS ? successImg : failImg}
+        />
         <h2
-          className={`mt-4 font-bold ${type == MODAL_TYPES.SUCCESS  ?"text-green-500" : "text-red-500"} text-xl md:text-2xl text-center font-cairo`}
+          className={`mt-4 font-bold ${
+            type == MODAL_TYPES.SUCCESS ? "text-green-500" : "text-red-500"
+          } text-xl md:text-2xl text-center font-cairo`}
         >
           {title}
         </h2>
       </div>
-      
+
       {/* Message */}
       <p className="mt-6 text-center text-gray-700 text-base md:text-lg font-cairo">
         {message}
@@ -35,7 +42,14 @@ const   StatusModal = ({ type, onConfirm, onClose, title, message }) => {
 
       {/* Button */}
       <div className="flex justify-center items-center mt-8">
-        <Button onClick={onConfirm || onClose} icon={<CorrectCircle color="#E89B32" fill="black" />} text={t('common.ok')} />
+        <Button
+          onClick={() => {
+            onConfirm();
+            onClose();
+          }}
+          icon={<CorrectCircle color="#E89B32" fill="black" />}
+          text={t("common.ok")}
+        />
       </div>
     </div>
   );
